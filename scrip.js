2a1fc905498906637c177b437edff37776b1f63e
Aigul <sharipova.aig@gmail.com>
2022-07-18 19:11:42 -0400

    js

--- scrip.js
+++ scrip.js
@@ -1,11 +1,14 @@
 let text = "I am a software engineer...";
 let i = 0;
+
 let speed = 100;
 
 function type() {
   if (i < text.length) {
     document.querySelector('#par').textContent += text.charAt(i);
+    
     i++;
+
     setTimeout(type, speed)
   }
 }
